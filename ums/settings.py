8bb5065cb7e7c3982182eff58cb6bd0d6af05568
Ding Ding Ding Jack Yang <jacky.jingyi@gmail.com>
2022-07-29 14:54:48 +0800

    Update settings.py

--- ums/settings.py
+++ ums/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ums1',
         'USER': 'root',
-        'PASSWORD': 'ew3402123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
